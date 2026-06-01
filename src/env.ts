03b55f89f1a6164efa2467211e5344103c244614
Emmanuel Xs <emmanuelxs101@gmail.com>
2026-06-01 16:04:28 +0100

    feat: Add environment variables and update authentication for Google and GitHub

--- src/env.ts
+++ src/env.ts
@@ -7,6 +7,10 @@ export const env = createEnv({
     DATABASE_URL: z.url(),
     BETTER_AUTH_SECRET: z.string().min(1),
     BETTER_AUTH_URL: z.url(),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1),
   },
 
   /**
@@ -29,6 +33,12 @@ export const env = createEnv({
   DATABASE_URL: process.env.DATABASE_URL,
   BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
   BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
 
   VITE_APP_TITLE: import.meta.env.VITE_APP_TITLE,
   VITE_PUBLIC_URL: import.meta.env.VITE_PUBLIC_URL,
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,22 @@
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { tanstackStartCookies } from 'better-auth/tanstack-start';
-import { betterAuth } from 'better-auth';
+import { betterAuth } from 'better-auth/minimal';
 
 import { db } from '@/db';
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
   },
   database: drizzleAdapter(
     db, {
